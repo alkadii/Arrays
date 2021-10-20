f410aa620b35b805cf8eddebee149bc4094aa82a
khaled <alkadi.khaled@hotmail.com>
2021-10-20 14:35:30 +0200

    arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,10 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            // ملاحظة هامة 
+            // Variable var und object für alle Variable-Typen
+            
+
             #region Arrays (مصفوفات) Variable[] V-Name = new V-Type[V-Anzahl];
 
             //string[] str = new string[4];
@@ -48,7 +52,7 @@ namespace Arrays
 
             #endregion
 
-            #region Anzahl der Arrays 
+            #region Anzahl der Arrays Length
 
             //string[] nam = new string[2];
             //nam[0] = "Khaled"; nam[1] = "Alkadi";
@@ -57,31 +61,108 @@ namespace Arrays
             //Console.WriteLine(arrays);
 
 
+            //string[] str = new string[] { "Khaled", "Hasnaa", "Afraa", "Riad", "Ahmad" };
+
+            //Console.WriteLine(str.Length);
+
+
 
             #endregion
 
             #region إضافة عناصر المصفوفة بشكل أسهل 
+            // 1- Erste Methode 
 
             //int[] anzahl = { 10, 20, 30, 80, 90, 60, 45 };
             //Console.WriteLine(anzahl.Length);
             //Console.WriteLine(anzahl[2]);
 
+            //2- Zweite Methode 
+
+            //int[] arrays = new int[] { 57, 76, 5, 3, 2, 1, 8, 9, 05 };
+            //Console.WriteLine(arrays[8]);
+
+
+
+
             #endregion
 
-            #region
-            string[] strnames = new string[4];
-            strnames[0] = "Khaked";
-            strnames[1] = "Hasnaa";
-            strnames[2] = "Afraa";
-            strnames[3] = "Mohamad";
+            #region Arrays mit for
+            //string[] strnames = new string[4];
+            //strnames[0] = "Khaked";
+            //strnames[1] = "Hasnaa";
+            //strnames[2] = "Afraa";
+            //strnames[3] = "Mohamad";
 
 
-            for(int x = 0; x < strnames.Length;x++)
-            {
-                Console.WriteLine(strnames[x]);
-            }
+            //for (int x = 0; x < strnames.Length; x++)
+            //{
+            //    Console.WriteLine(strnames[x]);
+            //}
+
+            //int[] anzahl = new int[] { 90, 87, 67, 54, 32 ,45,32};
+
+            //for (int f = 0; f < anzahl.Length; f++) 
+            //{
+            //    Console.WriteLine(anzahl[f]);
+            //}
+
 
             #endregion
+
+            #region Array.indexof(Name von Variable,nuhmmer von Variable) رقم الفهرس
+
+            //int[] anzahl = new int[] { 95, 4, 3, 2, 5, 643, 2, 14, 65 };
+
+            //Console.WriteLine(Array.IndexOf(anzahl,643));
+
+
+
+            #endregion
+
+            #region Array.sort(Name von Variable); ترتيب الاعداد في (الاريي) تصاعديا
+
+            //int[] an = new int[] {200, 65, 45, 345, 745, 34, 65 };
+
+            //Array.Sort(an);
+            //for (int h = 0; h < an.Length; h++)
+            //{
+            //    Console.WriteLine(an[h]);
+
+            //}
+
+
+
+            #endregion
+
+            #region Array.reverse(Name von Variable); ترتيب الاعداد في (الاريي) عكسيا
+            //int[] an = new int[] {76,45,42,65,87,33};
+            //Array.Reverse(an);
+            //for (int h = 0; h < an.Length; h++)
+            //{
+
+            //    Console.WriteLine(an[h]);
+            //}
+
+            #endregion
+
+            #region Array.sort(Name von Variable); dann Array.reverse(Name von Variable); ترتيب الاعداد في (الاريي) تنازليا
+
+            //int[] an = new int[] { 76, 45, 42, 65, 87, 33 };
+            //Array.Sort(an);
+            //Array.Reverse(an);
+
+            //for (int d = 0; d < an.Length; d++)
+            //{
+            //    Console.WriteLine(an[d]);
+            //}
+
+
+
+            #endregion
+
+
+
+
         }
 
 
